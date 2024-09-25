b10a3cc84653650bea3efdca029e200167ae34d2
sandamith9 <141568174+sandamith9@users.noreply.github.com>
2024-09-25 15:06:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://www.vecteezy.com/free-photos/alone-
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-SUDO_NB: process.env.SUDO_NB || "94704227534",
+SUDO_NB: process.env.SUDO_NB || "94717937870",
 OWNER_NAME: process.env.OWNER_NAME || "no name",
 BOT_NAME: process.env.BOT_NAME || "I'm alone",
 AUTO_VOICE: process.env.AUTO_VOICE || "false"
